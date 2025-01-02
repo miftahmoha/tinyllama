a6be2f00a261db5789e18c0cfaee6a9c3591393c
miftahmoha <m.miftah.achraf@gmail.com>
2025-01-02 10:11:01 +0100

    Global variables fix.

--- tinyllama/globals.py
+++ tinyllama/globals.py
@@ -1,5 +1,4 @@
 from os import getenv
-from typing import cast
 
 import torch
 
@@ -7,6 +6,5 @@ import torch
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # set environment vars
-# [TODO] doesn't seem to work
-DISABLE_LOGS: bool = cast(bool, getenv("DISABLE_LOGS", 1))
-DISABLE_TQDM: bool = cast(bool, getenv("DISABLE_TQDM", 0))
+DISABLE_LOGS: bool = bool(int(getenv("DISABLE_LOGS", 1)))
+DISABLE_TQDM: bool = bool(int(getenv("DISABLE_TQDM", 0)))
